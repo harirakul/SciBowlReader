ac52405a7c021190e8131e4f5ea6522613b0ba4a
Aditya Matam <aditya0310@outlook.com>
2021-01-21 17:14:09 -0800

    scraped ALL scibowl packets and storing in byte format

---
+++ dumper.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import pickle
+from reader import Packet
+
+with open('res\SciBowl.html', 'r') as f:
+    ht = f.read()
+    soup = BeautifulSoup(ht)
+
+    group = 0
+    prefix = "https://science.osti.gov"
+    for ul in soup.find_all('ul'):
+        group += 1
+        rnd = 0
+        for li in ul.find_all('li'):
+            rnd += 1
+            url = prefix + li.find('a')['href']
+
+            p = Packet(url)
+            with open(f"sets/group{group}/round{rnd}.txt", 'wb') as out:
+                pickle.dump(p, out)
